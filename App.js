93334ae8c1d7843f3dcc7d58b86b51953e76927c
edsonjunior <edsondelimajunior@gmail.com>
2019-03-25 17:01:19 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import Header from './src/components/Header'
+import Post from './src/components/Post'
+
+export default class App extends Component {
+	render() {
+		return (
+			<View>
+				<Header />
+				<Post image={require('./assets/imgs/fence.jpg')} />
+			</View>
+    	)
+	}
+}
